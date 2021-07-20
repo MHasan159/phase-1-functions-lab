7f972301834a7f2e278b0ce0ed3b16852a1a54dd
MohammadHasan <rony15987@yahoo.com>
2021-07-20 11:03:03 -0400

    commit tree dd65cdbd71afbc0d47e8356d60a80d0ad4a0d49f

--- index.js
+++ index.js
@@ -1,1 +1,48 @@
 // Code your solution in this file!
+//function  distanceFromHqInBlocks(block){
+  //  if(block>=42){
+       // return block-42;
+   // }
+  //  else{
+        //42-block;
+    //}
+//}
+//function  distanceFromHqInFeet(block){
+    //let numOfBlocks = distanceFromHqInBlocks(block);
+    //return numOfBlocks*264;
+//}
+//function distanceTravelledInFeet()
+const hq = 42
+const feet = 264
+function distanceFromHqInBlocks(destination){
+  if (destination >= 42){
+      return destination - hq
+    }
+  else if (destination <= 42) {
+      return hq - destination
+    }
+ }
+ function distanceFromHqInFeet(destination){
+   return distanceFromHqInBlocks(destination)*feet
+ }
+ 
+ function distanceTravelledInFeet (headquarters, num2){
+    if (num2 > headquarters){
+        return (num2 - headquarters)*feet;
+    }else{
+    return (headquarters - num2)*feet;
+    }
+}
+function calculatesFarePrice(start, destination) {
+    const totalFeet = distanceTravelledInFeet(start, destination)
+    let cost
+    if (totalFeet < 400) {
+        return 0;
+    } else if ((totalFeet >= 400) && (totalFeet <= 2000)) {
+        return cost = (totalFeet - 400) * .02
+    } else if (totalFeet > 2500) {
+        return 'cannot travel that far'
+    } else if (totalFeet > 2000) {
+        return 25
+    } 
+}
